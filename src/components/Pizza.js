871d56be4246d5f71381af985f7b1202c98629cb
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-14 11:04:04 -0500

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,14 +1,45 @@
 import React from "react"
 
-const Pizza = () => {
-  return(
+
+
+
+export default class Pizza extends React.Component {
+handleClick = () => {
+  this.props.editPizza(this.props.pizza)
+}
+render() {
+  return (
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{this.props.pizza.topping}</td>
+      <td>{this.props.pizza.size}</td>
+      <td>{this.props.pizza.vegetarian ? "Yes" : "No"}</td>
+      <td><button onClick={this.handleClick} type="button" className="btn btn-primary">Edit Pizza</button></td>
     </tr>
   )
 }
 
-export default Pizza
+
+  
+}
+// const handleClick = (props) => {
+//   props.editPizza(props.pizza)
+// }
+
+
+// const Pizza = (props) => {
+//   // console.log(this.props)
+//   // console.log(props)
+ 
+//   return(
+//     <tr>
+//       <td>{props.pizza.topping}</td>
+//       <td>{props.pizza.size}</td>
+//       <td>{props.pizza.vegetarian ? "Yes" : "No"}</td>
+//       <td><button onClick={handleClick} type="button" className="btn btn-primary">Edit Pizza</button></td>
+//     </tr>
+//   )
+// }
+
+
+
+// export default Pizza
